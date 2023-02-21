5dd1ef0f8eed469383d0c7f923becee2d8aa0a28
itaivincent <itaivincent321@gmail.com>
2023-02-21 17:53:51 +0200

    changes to the employee view

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -14,8 +14,7 @@
                       <option value="AL">Alabama</option>
                       <option value="WY">Wyoming</option>
                       <option value="AM">America</option>
-                      <option value="CA">Canada</option>
-                      <option value="RU">Russia</option>
+                      <option value="CA">Canada</option>                
                     </select>
                   </div>
                   <div class="form-group">
@@ -25,7 +24,6 @@
                       <option value="WY">Wyoming</option>
                       <option value="AM">America</option>
                       <option value="CA">Canada</option>
-                      <option value="RU">Russia</option>
                     </select>
                   </div>
                 </div>
