a03e4a8de2418a51fd8ef15727d8885eeacba291
itaivincent <itaivincent321@gmail.com>
2023-03-06 23:23:48 +0200

    assets

--- resources/views/assets/create.blade.php
+++ resources/views/assets/create.blade.php
@@ -10,26 +10,26 @@
               <div class="col-12 grid-margin">
               <div class="card">
                 <div class="card-body">
-                  <h4 class="card-title">create asset record</h4>
+                  <h4 class="card-title">Create Asset Record</h4>
                   <form class="form-sample" action="{{route('assets.store')}}" method="POST">
                     @csrf
                     <p class="card-description">
-                      Personal info
+                      Asset Info
                     </p>
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">First Name</label>
+                          <label class="col-sm-3 col-form-label">Asset Name</label>
                           <div class="col-sm-9">
                             <input type="text" name="name" class="form-control" required/>
                           </div>
                         </div>
-                      </div>
+                      </div>  
                       <div class="col-md-6">
                         <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">Last Name</label>
+                          <label class="col-sm-3 col-form-label">Asset Code</label>
                           <div class="col-sm-9">
-                            <input type="text" name="surname"  class="form-control" required/>
+                            <input type="text" name="code"  class="form-control" required/>
                           </div>
                         </div>
                       </div>
@@ -37,20 +37,26 @@
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">Gender</label>
+                          <label class="col-sm-3 col-form-label">Asset Type</label>
                           <div class="col-sm-9">
-                            <select  name="sex" class="form-control">
-                              <option value="M">Male</option>
-                              <option value="F">Female</option>
+                            <select  name="type" class="form-control">
+                              <option value="Laptop">Laptop</option>
+                              <option value="Desktop">Desktop</option>
+                              <option value="Hard Drive">Hard Drive</option>
+                              <option value="Monitor">Monitor</option>
+                              <option value="CPU">CPU</option>
+                              <option value="PowerPack">PowerPack</option>
+                              <option value="Mouse">Mouse</option>
+                              <option value="Other">Other</option>
                             </select>
                           </div>
                         </div>
                       </div>
                       <div class="col-md-6">
                         <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">Date of Birth</label>
+                          <label class="col-sm-3 col-form-label">Date</label>
                           <div class="col-sm-9">
-                            <input type="date" name="dob" class="form-control" placeholder="dd/mm/yyyy" required/>
+                            <input type="date" name="date" class="form-control" placeholder="dd/mm/yyyy" required/>
                           </div>
                         </div>
                       </div>
@@ -58,47 +64,24 @@
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">Department</label>
+                          <label class="col-sm-3 col-form-label">Description</label>
                           <div class="col-sm-9">
-                            <select name="department" class="form-control" required>
-                              <option value="1">Category1</option>
-                              <option value="2">Category2</option>
-                              <option value="3">Category3</option>
-                              <option value="4">Category4</option>
-                            </select>
+                            <input type="text" name="desc" class="form-control"  required/>
                           </div>
                         </div>
                       </div>
 
                       <div class="col-md-6">
                         <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">National Id</label>
+                          <label class="col-sm-3 col-form-label">Brand</label>
                           <div class="col-sm-9">
-                            <input type="type" name="nationalId" class="form-control" placeholder="national Id"  required/>
+                            <input type="text" name="brand" class="form-control"  required/>
                           </div>
                         </div>
                       </div>                  
                     </div>
 
-                    <div class="row">
-                      <div class="col-md-6">
-                        <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">Pay Number</label>
-                          <div class="col-sm-9">
-                            <input type="text" name="empNumber" class="form-control" required/>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="col-md-6">
-                        <div class="form-group row">
-                          <label class="col-sm-3 col-form-label">Start Date</label>
-                          <div class="col-sm-9">
-                            <input type="date" name="startDate" class="form-control" placeholder="dd/mm/yyyy"  required/>
-                          </div>
-                        </div>
-                      </div>
-                    </div>             
+                             
                     <button type="submit" class="btn btn-primary btn-lg">Save</button>
                     <button class="btn btn-lightbtn-lg">Cancel</button>
                   </form>
