c26f3349cb0f905a385bff92165dbd38f927696b
itaivincent <itaivincent321@gmail.com>
2023-03-10 01:33:15 +0200

    assets

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\User;
 
 class UserTest extends TestCase
 {
@@ -11,8 +12,46 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+
+     public function test_login_form(){
+
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+     }
+
+
+     public function test_user_duplication(){
+
+        $user1 = User::make([
+            'name' => 'Ray',
+            'email' => 'ray@gmail.com',
+         
+        
+        ]);
+
+        $user2 = User::make([
+            'name' => 'John',
+            'email' => 'john@gmail.com',
+           
+        
+        ]);
+
+        $this->assertTrue($user1->name != $user2->namespace);
+     }
+
+     public function test_it_stores_new_users(){
+
+        $this->withoutMiddleware();
+        $response = $this->post('/register',[
+            'name' => 'Rexi',
+            'email' => 'rexi@gmail.com', 
+            'password' => '12345u678',
+            'password_confirmation' => '12345u678'
+        ]);
+
+        
+        $response->assertRedirect('/home');
+     }
+      
 }
