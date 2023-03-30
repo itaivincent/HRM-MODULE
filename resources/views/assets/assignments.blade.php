cbd781ac2f81f96d682d519de1f8e9bc79537ec0
itaivincent <itaivincent321@gmail.com>
2023-03-30 23:11:02 +0200

    adding assignments

--- resources/views/assets/assignments.blade.php
+++ resources/views/assets/assignments.blade.php
@@ -7,19 +7,39 @@
               <div class="col-md-6 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">   
-                  <form method="post" class="form-sample" action="/employees/{{$employee->id}}"  />
+                  <form method="post" class="form-sample" action=""  />
                     @csrf
                     @method('put')
-                  <h4 class="card-title">Change Employment Status</h4>
+                  <h4 class="card-title">Please select an employee </h4>
                   <div class="form-group">
                     <label><b>You can change the status of an employee on the dropdown below</b></label>
                     <hr></hr>
                     <select class="js-example-basic-single w-100" name="status">
                       <option value="1">Active</option> 
-                      <option value="2">Terminated</option>
-                      <option value="3">Resigned</option>
-                      <option value="4">Retired</option>   
-                      <option value="5">On Leave</option>                                               
+                                                                  
+                    </select>
+                  </div>
+                  <div>
+           
+                </div>  
+               
+                </div>
+               
+              </div>
+            </div>
+
+                   
+            <div class="col-md-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">   
+             
+                  <h4 class="card-title">Please select an asset</h4>
+                  <div class="form-group">
+                    <label><b>You can change the status of an employee on the dropdown below</b></label>
+                    <hr></hr>
+                    <select class="js-example-basic-single w-100" name="status">
+                      <option value="1">Active</option> 
+                                                                 
                     </select>
                   </div>
                   <div>
@@ -31,73 +51,7 @@
                
               </div>
             </div>
-
-            
-            <div class="col-md-6 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">Personal Details</h4>
-                  <p class="card-description">
-                  <b> Summary of the employee' records </b>
-                  </p>
-                  <hr></hr>
-                  <div class="form-group row">
-                    <div class="col">
-                      <label>Name</label>
-                      <div id="the-basics">
-                        <input class="typeahead" type="text"value="{{$employee->name}} {{$employee->surname}}" disabled/>
-                      </div>
-                    </div>
-                    <div class="col">
-                      <label>Department</label>
-                      <div id="bloodhound">
-                        <input class="typeahead" type="text" value="{{$employee->department_id}}" disabled>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="form-group row">
-                    <div class="col">
-                      <label>PayNumber</label>
-                      <div id="the-basics">
-                        <input class="typeahead" type="text" value="{{$employee->employeeNumber}}" disabled>
-                      </div>
-                    </div>
-                    <div class="col">
-                      <label>Start Date</label>
-                      <div id="bloodhound">
-                        <input class="typeahead" type="text" value="{{$employee->startDate}}" disabled>
-                      </div>
-                    </div>
-                  </div>
-
-
-                  <div class="form-group row">
-                  <label class="col-sm-4 col-form-label">Employment Status:</label>
-                          <div class="col-sm-3">
-                            <div class="form-check">
-                              <label class="form-check-label">
-                                @if($employee->status == 1)
-                              <button type="button" class="btn btn-success btn-rounded btn-fw">Active</button>
-                              @elseif($employee->status == 2)
-                              <button type="button" class="btn btn-danger btn-rounded btn-fw">Terminated</button>
-                              @elseif($employee->status == 3)
-                              <button type="button" class="btn btn-danger btn-rounded btn-fw">Resigned</button>
-                              @elseif($employee->status == 4)
-                              <button type="button" class="btn btn-danger btn-rounded btn-fw">Retired</button>
-                              @else
-                              <button type="button" class="btn btn-danger btn-rounded btn-fw">On Leave</button>
-                              @endif
-                              </label>
-                           </div>
-                        </div>
-                    
-
-                  </div>
-
-                </div>
-              </div>
-            </div>
+      
           </div>
         </div>
              
